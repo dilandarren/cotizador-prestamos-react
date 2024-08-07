13371d3a7d1debc3cfdcbe3c0aaaa4d43b0c4267
Dilan <dilandarren1@gmail.com>
2024-08-07 22:19:09 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+
+function App() {
+
+  return (
+    <>
+      <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+
+        
+        <Header />
+      </div>
+    </>
+  )
+
+}
+
+export default App
