a82dada8ba9219ff1efa14e2e3a772718541cb18
Dilan <dilandarren1@gmail.com>
2024-08-08 21:38:38 +0200

    add select

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,97 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Button from "./components/Button";
+import { formatearDinero } from "./helpers";
 
 function App() {
+
+  const [cantidad, setCantidad] = useState(10000);
+
+  const [meses, setMeses] = useState(6);
+
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100
+
+  function handleChange(e) {
+    setCantidad( Number(e.target.value));
+  }
+
+
+  function handleChangeDecremento() {
+    const valor = cantidad - STEP;
+
+
+    if (valor < MIN) {
+      alert('Cantidad no válida');
+      return;
+    }
+
+
+    setCantidad(valor);
+  }
+
+  function handleChangeIncremento() {
+    const valor = cantidad + STEP;
+
+
+    if (valor > MAX) {
+      alert('Cantidad no válida');
+      return;
+    }
+
+
+    setCantidad(valor);
+  }
 
   return (
     <>
       <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-
-        
         <Header />
+
+      <div className="flex justify-between my-6">
+        <Button 
+          operador='-'
+          fn={handleChangeDecremento}
+        />
+
+        <Button 
+          operador='+'
+          fn={handleChangeIncremento}
+        />
+      </div>
+
+
+
+        <input
+         type="range" 
+         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+         onChange={handleChange}
+         min={MIN}
+         max={MAX}
+         step={STEP}
+         value={cantidad}        
+        />
+
+        <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+          {formatearDinero(cantidad)}
+        </p>
+
+        <h2 className='text-center text-2xl font-extrabold text-gray-600'>
+          Elige un <span className="text-indigo-600">Plazo</span> a Pagar
+        </h2>
+
+        <select
+        
+        className="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500 mt-5"
+        value={meses}
+        onChange={ e => setMeses(e.target.value)}
+        >
+          <option value="6">6 Meses</option>
+          <option value="12">12 Meses</option>
+          <option value="24">24 Meses</option>
+      </select>
       </div>
     </>
   )
